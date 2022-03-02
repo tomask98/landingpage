5f5bf329b063ce9add7c9f1f08a2b7c406bf1b57
tomas kohler <91294872+tomask98@users.noreply.github.com>
2022-03-02 14:27:56 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,32 @@
+window.sr = ScrollReveal();
+  sr.reveal('.main', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-100px'
+  });
+  window.sr = ScrollReveal();
+  sr.reveal('.text', {
+    duration: 4000,
+    origin: 'left',
+    distance: '-100px'
+  });
+  window.sr = ScrollReveal();
+  sr.reveal('.serv1', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-100px'
+  });
+  window.sr = ScrollReveal();
+  sr.reveal('.serv2', {
+    duration: 3000,
+    origin: 'left',
+    distance: '-100px'
+  });
+  window.sr = ScrollReveal();
+  sr.reveal('.flecha', {
+    duration: 3000,
+    origin: 'top',
+    distance: '100px'
+    
+  });
+  
